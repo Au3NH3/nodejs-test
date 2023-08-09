25db39ed53a78a03b6aa84c4ef2fce48c44de265
andy <a@a>
2023-08-09 19:21:12 +0800

    first commit

---
+++ public/func.js
@@ -0,0 +1,29 @@
+// var dbg = 1;
+const form = document.querySelector("form");
+// const cmd = document.querySelector("#run");
+const input = document.querySelector("input");
+const output = document.querySelector("#out");
+const clear = document.querySelector(".clear");
+const stat = document.querySelector("#stat");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	//var	fd = new FormData(form);
+	//fetch("/run?, {body:fd}")//.then(res => res.json()).then(json => console.log(json));
+	// fetch("/run?" + input.name + "=" + input.value).then(
+		// res => res.json()).then(json => console.log(json));
+	fetch("/run?" + input.name + "=" + input.value).then(
+		res => res.text()).then(text => {
+			stat.textContent = "output:"
+			output.textContent += text
+			});
+	//console.log(input.name, input.value);
+	// output.textContent = "running...";
+	stat.textContent = "running...";
+	output.textContent = "$" + input.value;
+});
+
+clear.addEventListener("click", ()=>{
+	input.value = "";
+	output.textContent = "$";
+})
